d0240fcec86784784694d1bdde2e7a5ed235e02a
YapieRT <sokolval555@gmail.com>
2021-07-14 20:52:29 +0300

    update 14 jul

--- db_dump.sh
+++ db_dump.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-database_name=moodle_backup #name of your backup
+database_needed_to_be_dumped=moodle # database that you need to backup
 
-current_date=`date +%d-%m-%Y_%H-%M`
+database_backup_name=moodle_backup #name of your backup
 
 USER_DUMP= # your sql user of database
 
 PASSWORD_DUMP= # password of this user
 
-ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/" #Directory where script is located
+current_date=`date +%d-%m-%Y_%H-%M`
+
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/" #Directory where script is located 
 
 status_mysql=`systemctl status mysql | grep running`
 
 #====================
 
-backup_name=$database_name"_"$current_date".sql"
+backup_name=$database_backup_name"_"$current_date".sql"
 
 
 if [[ ${#status_mysql} == 0 ]];
@@ -25,7 +27,7 @@ else
 	echo "MySql service - running"
 fi
 
-dump=`mysqldump --single-transaction -u $USER_DUMP -p$PASSWORD_DUMP moodle > $ABSOLUTE_PATH$backup_name` # Creating backup in dir where script is located
+dump=`mysqldump --single-transaction -u $USER_DUMP -p$PASSWORD_DUMP $database_needed_to_be_dumped > $ABSOLUTE_PATH$backup_name` # Creating backup in dir where script is located
 
 #CRONTAB
 #*/1 * * * * {path}/db_dump.sh # dump every minute for test # use pwd in script location
